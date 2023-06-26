bb373207cdbb83295db5d412841c0d978bdeeaf2
naimzaaraoui <zaaraouinaim@gmail.com>
2023-06-26 21:09:26 +0100

    New Fashion Design

---
+++ js/main.js
@@ -0,0 +1,20 @@
+// let a = 1;
+// let b = "4";
+// console.log(a + b);
+// console.log(a + Number(b));
+
+// function sum(a) {
+//     return b => {
+//         return c => {
+//             return a * b * c;
+//         }
+//     }
+// }
+
+// let sum1 = sum(2);
+// let sum2 = sum1(5);
+// let sum3 = sum2(4);
+// console.log(sum3);
+
+// console.log(sum(2)(5)(4)); // 40
+
